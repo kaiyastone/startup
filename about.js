6c07084675d985daf7487f695d612293189ef2ff
Kaiya Stone <enotsayiak@gmail.com>
2023-04-04 20:47:40 -0600

    working on using express and fetch

---
+++ about.js
@@ -0,0 +1,38 @@
+function randQuote(data) {
+    fetch('https://api.quoatable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+       const containerEl = document.querySelector('#quote');
+
+       const quoteEl = document.createElement('p');
+       quoteEl = document.createElement('p');
+       authorEl.classList.add('author');
+
+       quoteEl.textContent = data.content;
+       authorEl.textContent = data.author;
+
+       containerEl.appendChild(quoteEl);
+       containerEl.appendChild(authorEl);
+    });
+}
+
+randQuote();
+
+function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+        .then((response) => response.json())
+        .then((data) => {
+            const containerEl = document.querySelector('#picture');
+
+            const width = containerEl.offsetWidth;
+            const height = containerEl.offsetHeight;
+
+            const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+            const imgEl = document.createElement('img');
+            imgEl.setAttribute('src', imgUrl);
+            containerEl.appendChild(imgEl);
+        });
+}
+
+displayPicture;
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-application.use(express.static('public'));
+
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+app.use((_req, res) => {
+    res.sendFile('index.html', {root: 'public'});
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
